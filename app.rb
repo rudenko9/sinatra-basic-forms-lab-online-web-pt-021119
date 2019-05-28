6c99841587972c3e397f9b376d1e96499b9da76f
Nataliya R <n.rudenko@gmail.com>
2019-05-28 02:16:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,17 +5,11 @@ class App < Sinatra::Base
     erb :index
     
   end 
+  
   get '/new' do 
     erb :create_puppy
   end 
   
-  post '/puppy' do 
-     puppy = Puppy.new(params[:name], params[:breed], params[:age], )
-     @name = puppy.name
-    @breed = puppy.breed
-    @age = puppy.months_old
-    erb :display_puppy
-    
-  end 
+  
 
 end
