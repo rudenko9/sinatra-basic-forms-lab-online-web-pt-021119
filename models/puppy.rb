13896ce7b4dbedb967481c675236c6e849aa74f7
Nataliya R <n.rudenko@gmail.com>
2019-05-28 02:11:27 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,9 +1,9 @@
 class Puppy
-  attr_accessor :name, :breed, :age 
+  attr_accessor :name, :breed, :month_old
   def initialize
     @name = name
     @breed = breed 
-    @age = age
+    @month_old = month_old
   end 
   
 end 
