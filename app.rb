bf197f267a58bf9b4a716cb587a18a2d94fe9bb0
Nataliya R <n.rudenko@gmail.com>
2019-05-28 02:13:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end 
   
   post '/puppy' do 
-     puppy = Puppy.new(params[:name], params[:breed], params[:month_old], )
+     puppy = Puppy.new(params[:name], params[:breed], params[:age], )
      @name = puppy.name
     @breed = puppy.breed
     @age = puppy.months_old
